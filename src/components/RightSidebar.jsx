652756d3b77e23f6ef2c6ac8c23cace60290e0f6
Carlos Martinez <curlosmart@gmail.com>
2021-10-23 23:28:07 -0400

    Right sidebar

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,14 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import OnlineFriends from './OnlineFriends'
 
 const MainContainer = styled.div`
   flex: 2;
+  padding: 15px;
+`
+
+const BirthdayNotification = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const BirthdayIcon = styled.img`
+  width: 32px;
+  margin-right: 12px;
+`
+
+const Advertisement = styled.img`
+  width: 100%;
+  border-radius: 10px;
+  margin: 30px auto;
 `
 
 const RightSidebar = () => {
 
   return (
-    <MainContainer>hello</MainContainer>
+    <MainContainer>
+      <BirthdayNotification>
+        <BirthdayIcon src="/assets/gift.png" />
+        <div><strong>Pola Foster</strong> and <strong>3 other friends</strong> have a birthday today.</div>
+      </BirthdayNotification>
+
+      <Advertisement src="/assets/ad.png" />
+      <OnlineFriends />
+
+    </MainContainer>
   )
 }
 
